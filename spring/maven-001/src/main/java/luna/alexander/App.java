f5822a491168b68cf51bf863f02a9ca18568d0a3
Mycroft1891 <mycroft1891@gmail.com>
2020-04-27 19:29:37 +0200

    Add: logback logger dependency and configuration file

--- spring/maven-001/src/main/java/luna/alexander/App.java
+++ spring/maven-001/src/main/java/luna/alexander/App.java
@@ -1,7 +1,13 @@
 package luna.alexander;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private final static Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        log.info("Hello Info");
+        log.debug("Hello Debug");
     }
 }
